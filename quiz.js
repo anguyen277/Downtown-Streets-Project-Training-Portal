0b4beb75e263dcd0df0ee796a4eee89fa75d1485
anguyen277 <50789500+anguyen277@users.noreply.github.com>
2020-10-05 16:05:46 -0700

    Add files via upload
    I added more questions in the HTML file and functions for the JS file. Keep in mind you use these 2 files + CSS to see the HTML page work.

--- quiz.js
+++ quiz.js
@@ -1,3 +1,15 @@
 function check() {
-    document.write("pls work");
+    var c=0;
+    var q1=document.quiz.question1.value;
+    var q2=document.quiz.question2.value;
+    var q3=document.quiz.question3.value;
+    var q4=document.quiz.question4.value;
+    var q5=document.quiz.question5.value;
+    if (q1=="Yes") {c++}
+    if (q2=="Earth") {c++}
+    if (q3=="SJSU") {c++}
+    if (q4=="Blue") {c++}
+    if (q5=="Yes") {c++}
+
+        document.write(c);
 }
