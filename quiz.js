42ae9fdaaa2a44b96081e9e96e4a96863010777a
anguyen277 <50789500+anguyen277@users.noreply.github.com>
2020-10-11 21:53:08 -0700

    Add files via upload
    The results page shows roughly how much you missed. There's only 5 problems so I made it display if you get 3 or less questions right, you failed. If you get 4 or more right, you passed.

--- quiz.js
+++ quiz.js
@@ -1,3 +1,22 @@
 function check() {
-    document.write("pls work");
+    var c=0;
+    var q1=document.quiz.question1.value;
+    var q2=document.quiz.question2.value;
+    var q3=document.quiz.question3.value;
+    var q4=document.quiz.question4.value;
+    var q5=document.quiz.question5.value;
+    var result=document.getElementById('result');
+    var quiz=document.getElementById("quiz");
+    if (q1=="Yes") {c++}
+    if (q2=="Earth") {c++}
+    if (q3=="SJSU") {c++}
+    if (q4=="Blue") {c++}
+    if (q5=="Yes") {c++}
+    quiz.style.display="none";
+
+        if (c<=3) {
+            result.textContent="Your result is " + c + ". You scored 60 percent or less. Please retry."
+        } else {
+            result.textContent="Your result is " + c + ". You scored 80 percent or more. You have passed!"
+        }
 }
